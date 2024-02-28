0c5fd0a067e663462a28e2115c63e2c8884238a1
Charlie McCain <cmccain@intellum.com>
2024-02-28 14:13:47 -0500

    add optional img_tag for build script

--- build-image.sh
+++ build-image.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+: ${IMG_TAG:="latest"}
+
 docker buildx build \
   --platform linux/amd64 \
-  --tag intellum/k8s-wait-for:latest \
+  --tag intellum/k8s-wait-for:${IMG_TAG} \
   --push \
   .
